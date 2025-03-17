7bba6e4a1d5b26a1d7155dd72e9234ba9a510694
Lorena <lorena_gr@hotmail.es>
2025-03-17 13:36:22 +0100

    ajustes en topContainer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ const Header = () => {
     const [hoveredButton, setHoveredButton] = useState(null);
 
     return(
-        <div className="sticky top-0 bg-transparent z-50">
+        <div className="sticky top-0 bg-transparent z-50 hidden md:block">
             <div className="flex justify-center space-x-6 py-4">
                 {['INICIO', 'FUNCIONALIDADES', 'DESCARGA', 'CONTACTO'].map((text, index) => (
                     <button
                         key={index}
-                        className={`text-white ${
-                            hoveredButton !== null && hoveredButton !== index ? 'blur-sm' : ''
+                        className={`text-white cursor-pointer ${
+                            hoveredButton !== null && hoveredButton !== index ? 'my-blur' : ''
                         }`}
                         onMouseEnter={() => setHoveredButton(index)}
                         onMouseLeave={() => setHoveredButton(null)}
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
 const Title = () => {
-    return(
-        <div></div>
+    const lettersRef = useRef([]); 
+
+    // Animación de entrada con GSAP
+    useEffect(() => {
+        const letters = lettersRef.current;
+
+        gsap.set(letters, {
+            opacity: 0,
+            x: -50,
+            scale: 0.5,
+        });
+
+        gsap.to(letters, {
+            opacity: 1,
+            x: 0,
+            scale: 1,
+            duration: 0.3,
+            stagger: 0.15, 
+            ease: 'power2.out', 
+        });
+    }, []);
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex  ">
+                {'HOMEBASE'.split('').map((letter, index) => (
+                    <span
+                        key={index}
+                        ref={(el) => (lettersRef.current[index] = el)} // Asignar referencia
+                        className="text-white font-bold transition-colors duration-200 hover:text-[#00BCE7] title-font"
+                    >
+                        {letter}
+                    </span>
+                ))}
+            </div>
+        </div>
     );
-}
+};
 
 export default Title;
