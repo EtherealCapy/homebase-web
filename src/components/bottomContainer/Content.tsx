6e63b51642f9e5db6f4abfaac7bca44781a2ff2f
Lorena <lorena_gr@hotmail.es>
2025-05-12 09:37:38 +0200

    download + footer components

--- src/components/bottomContainer/Content.tsx
+++ src/components/bottomContainer/Content.tsx
@@ -1,12 +1,15 @@
-import { useRef, useEffect } from 'react';
 import Carousel from './Carousel';
 import Description from './Description';
+import Download from './Download';
+import Footer from './Footer';
 
 const Content = () => {
     return (
         <div className="my-black z-15 relative w-full h-screen content">
             <Description /> 
             <Carousel />
+            <Download/>
+            <Footer/>
         </div>
     );
 };
